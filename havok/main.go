e4b20549f96abc1999296c14e1815e1e729b6e44
Evan Hazlett <ejhazlett@gmail.com>
2014-07-11 00:54:57 -0400

    added root-subdomain option; refactoring

--- havok/main.go
+++ havok/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"strings"
@@ -16,6 +17,7 @@ var (
 	ETCD_MACHINES            []string
 	DOCKER_URL               string
 	ROOT_DOMAIN              string
+	ROOT_SUBDOMAIN           string
 	HOST_IP                  string
 	NAME_REGEX               string
 	RATE_LIMIT               int
@@ -25,7 +27,8 @@ var (
 	CONN_LIMIT_VARIABLE      string
 	eng                      *engine.Engine
 	log                      = logrus.New()
-	version                  = "0.4"
+	VERSION                  = "0.5"
+	SHOW_VERSION             bool
 )
 
 func waitForInterrupt() {
@@ -41,6 +44,7 @@ func waitForInterrupt() {
 func init() {
 	flag.StringVar(&DOCKER_URL, "docker", "unix:///var/run/docker.sock", "Docker URL")
 	flag.StringVar(&ROOT_DOMAIN, "root-domain", "local", "Root level domain")
+	flag.StringVar(&ROOT_SUBDOMAIN, "root-subdomain", "www", "Root level subdomain (i.e. www)")
 	flag.StringVar(&ETCD_MACHINES_CONNECTION, "etcd-machines", "http://127.0.0.1:4001", "comma separated list of etcd hosts")
 	flag.StringVar(&HOST_IP, "host-ip", "127.0.0.1", "Host IP for accessing containers")
 	flag.StringVar(&NAME_REGEX, "names", ".*", "Containers with name matching regex will get added to etcd")
@@ -49,14 +53,19 @@ func init() {
 	flag.IntVar(&RATE_LIMIT_BURST, "rate-limit-burst", 1, "Specify rate limit burst (default: 1)")
 	flag.IntVar(&CONN_LIMIT, "conn-limit", 0, "Specify connection limit (default: 0)")
 	flag.StringVar(&CONN_LIMIT_VARIABLE, "conn-limit-var", "client.ip", "Variable for connection limiting (default: client.ip)")
+	flag.BoolVar(&SHOW_VERSION, "version", false, "Show version")
 	flag.Parse()
 	// parse etcd to list
 	ETCD_MACHINES = strings.Split(ETCD_MACHINES_CONNECTION, ",")
 }
 
 func main() {
-	log.Infof("Havok %s", version)
-	eng = engine.NewEngine(DOCKER_URL, ETCD_MACHINES, ROOT_DOMAIN, HOST_IP, NAME_REGEX, RATE_LIMIT, RATE_LIMIT_VARIABLE, RATE_LIMIT_BURST, CONN_LIMIT, CONN_LIMIT_VARIABLE)
+	if SHOW_VERSION {
+		fmt.Println(VERSION)
+		return
+	}
+	log.Infof("Havok %s", VERSION)
+	eng = engine.NewEngine(DOCKER_URL, ETCD_MACHINES, ROOT_DOMAIN, ROOT_SUBDOMAIN, HOST_IP, NAME_REGEX, RATE_LIMIT, RATE_LIMIT_VARIABLE, RATE_LIMIT_BURST, CONN_LIMIT, CONN_LIMIT_VARIABLE)
 	eng.Run()
 	waitForInterrupt()
 }
